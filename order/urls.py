7091301bbf36aef86669e9e6e467f2c0926d3038
Amr Mahfouz <eng.csed.amr.mahfouz@gmail.com>
2017-10-12 11:22:37 +0200

    Login and logout Structure for the index page

--- order/urls.py
+++ order/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register, name='register'),
-    # url(r'^login_user/$', views.login_user, name='login_user'),
-    # url(r'^logout_user/$', views.logout_user, name='logout_user'),
+    url(r'^login_user/$', views.login_user, name='login_user'),
+    url(r'^logout_user/$', views.logout_user, name='logout_user'),
 ]
--- order/views.py
+++ order/views.py
@@ -3,6 +3,35 @@ from django.contrib.auth import authenticate,login,logout
 from .forms import UserForm
 
 
+def index(request):
+    if not request.user.is_authenticated():
+        return render(request, 'order/login.html')
+    else:
+        return render(request, 'order/index.html')
+
+
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    return render(request, 'order/login.html', {'form': form})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'order/index.html')
+            else:
+                return render(request, 'order/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'order/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'order/login.html')
+
+
 def register(request):
     form = UserForm(request.POST or None)
     if form.is_valid():
@@ -14,7 +43,7 @@ def register(request):
         user = authenticate(username=username,password=password)
         if user is not None and user.is_active:
             login(request,user)
-            #navigateToIndex
+            return render(request, 'order/index.html')
     return render(request,'order/register.html',{'form':form})
 
 
